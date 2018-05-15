7adcaaea1ef16dbaf12485388855a18e09c87525
STOLFIEDUARDO <38699786+STOLFIEDUARDO@users.noreply.github.com>
2018-05-15 16:35:38 -0300

    Começando lista 2

---
+++ WFAlista2/WFAlista2/Exercicio01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAlista2
+{
+    public partial class Exercicio01 : Form
+    {
+        public Exercicio01()
+        {
+            InitializeComponent();
+        }
+    }
+}

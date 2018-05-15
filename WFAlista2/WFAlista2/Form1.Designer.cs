f8f2481fbeee09f9b64ebfb3e0978e8df8ec9a1d
STOLFIEDUARDO <38699786+STOLFIEDUARDO@users.noreply.github.com>
2018-05-15 17:20:44 -0300

    Adicionando

--- WFAlista2/WFAlista2/Form1.Designer.cs
+++ WFAlista2/WFAlista2/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -83,6 +84,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -92,6 +94,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -101,6 +104,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -110,6 +114,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -119,6 +124,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -128,6 +134,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -137,6 +144,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -146,6 +154,7 @@
             this.button10.TabIndex = 9;
             this.button10.Text = "button10";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -155,6 +164,7 @@
             this.button11.TabIndex = 10;
             this.button11.Text = "button11";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -164,6 +174,7 @@
             this.button12.TabIndex = 11;
             this.button12.Text = "button12";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
@@ -173,6 +184,7 @@
             this.button13.TabIndex = 12;
             this.button13.Text = "button13";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
@@ -182,6 +194,7 @@
             this.button14.TabIndex = 13;
             this.button14.Text = "button14";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
@@ -191,6 +204,7 @@
             this.button15.TabIndex = 14;
             this.button15.Text = "button15";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
@@ -200,6 +214,7 @@
             this.button16.TabIndex = 15;
             this.button16.Text = "button16";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // button17
             // 
@@ -209,6 +224,7 @@
             this.button17.TabIndex = 16;
             this.button17.Text = "button17";
             this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
             // 
             // button18
             // 
@@ -218,6 +234,7 @@
             this.button18.TabIndex = 17;
             this.button18.Text = "button18";
             this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
             // 
             // button19
             // 
@@ -227,6 +244,7 @@
             this.button19.TabIndex = 18;
             this.button19.Text = "button19";
             this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
             // button20
             // 
@@ -236,6 +254,7 @@
             this.button20.TabIndex = 19;
             this.button20.Text = "button20";
             this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
             // 
             // button21
             // 
@@ -245,6 +264,7 @@
             this.button21.TabIndex = 20;
             this.button21.Text = "button21";
             this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
             // 
             // button22
             // 
@@ -254,6 +274,7 @@
             this.button22.TabIndex = 21;
             this.button22.Text = "button22";
             this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
             // 
             // button23
             // 
@@ -263,6 +284,7 @@
             this.button23.TabIndex = 22;
             this.button23.Text = "button23";
             this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
             // 
             // button24
             // 
@@ -272,6 +294,7 @@
             this.button24.TabIndex = 23;
             this.button24.Text = "button24";
             this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
             // 
             // button25
             // 
@@ -281,6 +304,7 @@
             this.button25.TabIndex = 24;
             this.button25.Text = "button25";
             this.button25.UseVisualStyleBackColor = true;
+            this.button25.Click += new System.EventHandler(this.button25_Click);
             // 
             // button26
             // 
@@ -290,6 +314,7 @@
             this.button26.TabIndex = 25;
             this.button26.Text = "button26";
             this.button26.UseVisualStyleBackColor = true;
+            this.button26.Click += new System.EventHandler(this.button26_Click);
             // 
             // Principal
             // 
--- WFAlista2/WFAlista2/Form1.cs
+++ WFAlista2/WFAlista2/Form1.cs
@@ -23,5 +23,156 @@ namespace WFAlista2
             exercicio01.Show();
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           /* Exercicio02 exercicio02 = new Exercicio02();
+            exercicio02.Show();*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /* Exercicio03 exercicio03 = new Exercicio03();
+            exercicio03.Show();*/
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /* Exercicio04 exercicio04 = new Exercicio04();
+            exercicio04.Show();*/
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            /* Exercicio05 exercicio05 = new Exercicio05();
+            exercicio05.Show();*/
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /* Exercicio06 exercicio06 = new Exercicio06();
+            exercicio06.Show();*/
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /* Exercicio07 exercicio07 = new Exercicio07();
+            exercicio07.Show();*/
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            /* Exercicio08 exercicio08 = new Exercicio08();
+            exercicio08.Show();*/
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            /* Exercicio09 exercicio09 = new Exercicio09();
+            exercicio09.Show();*/
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            /* Exercicio10 exercicio10 = new Exercicio10();
+            exercicio10.Show();*/
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            /* Exercicio11 exercicio11 = new Exercicio11();
+             exercicio11.Show();*/
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            /* Exercicio12 exercicio12 = new Exercicio12();
+            exercicio12.Show();*/
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            /* Exercicio13 exercicio13 = new Exercicio13();
+            exercicio13.Show();*/
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            /* Exercicio14 exercicio14 = new Exercicio14();
+            exercicio14.Show();*/
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            /* Exercicio15 exercicio15 = new Exercicio15();
+            exercicio15.Show();*/
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            /* Exercicio16 exercicio16 = new Exercicio16();
+            exercicio16.Show();*/
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            /* Exercicio17 exercicio17 = new Exercicio17();
+            exercicio17.Show();*/
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            /* Exercicio18 exercicio18 = new Exercicio18();
+            exercicio18.Show();*/
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            /* Exercicio19 exercicio19 = new Exercicio19();
+            exercicio19.Show();*/
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            /* Exercicio20 exercicio20 = new Exercicio20();
+            exercicio20.Show();*/
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            /* Exercicio21 exercicio21 = new Exercicio21();
+            exercicio21.Show();*/
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            /* Exercicio22 exercicio22 = new Exercicio22();
+            exercicio22.Show();*/
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            /* Exercicio23 exercicio23 = new Exercicio23();
+            exercicio23.Show();*/
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            /* Exercicio24 exercicio24 = new Exercicio24();
+            exercicio24.Show();*/
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            /* Exercicio25 exercicio25 = new Exercicio25();
+            exercicio25.Show();*/
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            /* Exercicio26 exercicio26 = new Exercicio26();
+            exercicio26.Show();*/
+        }
+
     }
 }
